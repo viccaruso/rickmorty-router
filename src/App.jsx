fa251262b736e040d25dd67492c8f4d63d040eb1
Victor Caruso <vic.caruso@gmail.com>
2022-05-03 14:17:40 -0700

    Create CharacterCard and CharacterList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
+import CharacterList from './views/CharacterList';
 
 export default function App() {
   return (
     <Switch>
       <Route path="/characters/:id">
-        <p>You have switched to specific route</p>
+        <Link to="/">{'< All Characters'}</Link>
+        <p>Character detail goes here</p>
       </Route>
-      <Route path="/">
-        <p>You have switched to the root route</p>
+      <Route path="/characters">
+        <CharacterList />
+      </Route>
+      <Route exact path="/">
+        <Redirect to="/characters" />
       </Route>
     </Switch>
   );
