f039d163a3716c13e4307654204140c690713a89
Victor Caruso <vic.caruso@gmail.com>
2022-05-03 14:35:43 -0700

    Fetch characters on load and render nameplate component for each character

--- src/views/CharacterList.jsx
+++ src/views/CharacterList.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  useHistory,
+  useLocation,
+} from 'react-router-dom/cjs/react-router-dom.min';
+import CharacterNameplate from '../components/CharacterNameplate';
+import fetchCharacters from '../services/api';
 
 export default function CharacterList() {
-  return <div>CharacterList</div>;
+  const history = useHistory();
+  const location = useLocation();
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetch() {
+      setLoading(true);
+      const chars = await fetchCharacters();
+      setCharacters(chars);
+      setLoading(false);
+    }
+    fetch();
+  }, []);
+  return characters.map((char, index) => (
+    <CharacterNameplate key={index} character={char} />
+  ));
 }
