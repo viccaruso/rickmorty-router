adac88dc2d4df0009811ad9a130ce41a588b8b5e
Victor Caruso <vic.caruso@gmail.com>
2022-05-03 15:25:38 -0700

    Add link to nameplate

--- src/components/CharacterNameplate.jsx
+++ src/components/CharacterNameplate.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './nameplate.css';
 
 export default function CharacterNameplate({ character }) {
   console.log('character', character);
   return (
     <>
-      <div className={styles.nameplate}>
-        <h3>{character.name}</h3>
-        <p>{character.species}</p>
-        <img src={character.image} alt={`An image of ${character.name}`} />
-      </div>
+      <Link to={`/characters/${character.id}`}>
+        <div className={styles.nameplate}>
+          <h3>{character.name}</h3>
+          <p>{character.species}</p>
+          <img src={character.image} alt={`An image of ${character.name}`} />
+        </div>
+      </Link>
     </>
   );
 }
